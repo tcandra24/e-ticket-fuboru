2cd31cc82c39e6d40de1a5ec07f01f9d00c71ecf
tcandra24 <tcandra007@gmail.com>
2024-09-12 08:52:20 +0700

    Rev. Update Finish Page

--- resources/views/participant/transactions/finish.blade.php
+++ resources/views/participant/transactions/finish.blade.php
@@ -17,6 +17,20 @@
                                             <b>Drama Musikal SAILOR'S BIBLE</b>
                                         </p>
                                         <p class="text-center">
+                                            QR code yang berisi nomer kursi akan dikirimkan melalui nomer WA yang terdaftar
+                                            saat pembelian pada hari H-3.
+                                            <br>
+                                            Mohon disimpan dengan baik dan ditukarkan dengan tanda masuk pada hari H di meja
+                                            registrasi yang tersedia.
+                                        </p>
+                                        <p class="text-center">
+                                            Apabila ada pertanyaan seputar ticketing, dapat menguhubungi:
+                                            <br>
+                                            <b>0812-2958-2025</b>
+                                            <br>
+                                            <b>0813-3773-2732</b>
+                                        </p>
+                                        <p class="text-center">
                                             Acara ini dipersembahkan oleh:
                                         </p>
                                         <div class="d-flex justify-content-center" style="gap: 5px;">
@@ -29,12 +43,7 @@
                                             <img src="{{ asset('/assets/images/presented-1.png') }}" width="250"
                                                 class="img-fluid p-2" alt="Logo 1">
                                         </div>
-                                        <p class="text-center">
-                                            Konfirmasi pesanan akan dikirimkan ke nomer terdaftar.
-                                            Apabila ada pertanyaan seputar ticketing, dapat menguhubungi
-                                            <b>0812-2958-2025</b>
-                                        </p>
-                                        <div class="d-flex justify-content-center" style="gap: 5px;">
+                                        <div class="d-flex justify-content-center my-3" style="gap: 5px;">
                                             <a class="btn btn-primary" href="{{ route('participant.index') }}">
                                                 Beranda
                                             </a>
