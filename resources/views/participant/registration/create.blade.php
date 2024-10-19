df0289fbe2e463672297134161da0fad2734c46f
tcandra24 <tcandra007@gmail.com>
2024-10-19 16:50:32 +0700

    Disabled mapping Seat

--- resources/views/participant/registration/create.blade.php
+++ resources/views/participant/registration/create.blade.php
@@ -167,10 +167,10 @@
                             </div>
                             <button type="submit" class="btn btn-primary btn-submit">Submit</button>
                             <button type="reset" class="btn btn-danger">Reset</button>
-                            <a href="{{ asset('assets/images/layout-seat.jpg') }}" class="btn btn-info my-2"
+                            {{-- <a href="{{ asset('assets/images/layout-seat.jpg') }}" class="btn btn-info my-2"
                                 data-lightbox="Mapping Kursi Acara">
                                 Mapping Tempat Duduk
-                            </a>
+                            </a> --}}
                         </form>
                     </div>
                 </div>
